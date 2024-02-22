364e0bfa74e62bc0c479117b2fb5a4f1fbec8ab3
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-23 02:09:27 +0530

    bcrypt used for encryption

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const Expense = require("./model/expense");
 const User = require("./model/user");
@@ -48,39 +49,41 @@ app.post("/user/sign-up", async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     if (name.length == 0 || email.length == 0 || password.length == 0) {
-      return res.status(400).json({ error: "invalid params" });
+      return res.status(400).json({ err: "Bad params!" });
     }
-    const data = await User.create({ name, email, password });
-    res.status(200).json({ data });
+    bcrypt.hash(password, 10, async (err, hash) => {
+      console.log(err);
+      const data = await User.create({ name, email, password: hash });
+      res.status(201).json({ message: "Successfully signed up!" });
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error });
+    res.status(500).json(error);
   }
 });
 
 app.post("/user/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    // const user = await User.findOne({
-    //   where: { email: email, password: password },
-    // });
-    // if (user) {
-    //   res.status(200).json({ message: "Logged In!" });
-    // } else {
-    //   throw new Error("Invalid Credentials");
-    // }
-    const user= await User.findOne({where:{email:email}})
-    if(!user){
-      res.status(404).json({msg:"no user found"})
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      res.status(404).json({ msg: "no user found" });
     }
-    if(user.password===password){
-     return res.status(200).json({message:"Logged in!"})
-    }
-    else{
-      return res.status(400).json({msg:"invalid password"})
-    }
+    bcrypt.compare(password, user.password, (error, result) => {
+      if (error) {
+        res
+          .status(400)
+          .json({ success: "false", message: "Something went wrong!" });
+      }
+      if (result === true) {
+        res.status(200).json({ success: true, message: "Logged in!" });
+      }
+      if (result === false) {
+        res.status(200).json({ success: false, message: "Invalid password" });
+      }
+    });
   } catch (error) {
-    res.status(404).json({err:error.message})
+    res.status(404).json({ err: error.message });
   }
 });
 
