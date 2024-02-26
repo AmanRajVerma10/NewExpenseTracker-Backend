f52b98abb282ffe286f027ffc6553d59fe44a4df
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-27 01:20:16 +0530

    Optimisation using joins

--- controller/premiumFeature.js
+++ controller/premiumFeature.js
@@ -4,32 +4,18 @@ const sequelize = require("../util/database");
 
 exports.getUserLeaderboard = async (req, res, next) => {
   try {
-    const users = await User.findAll({ attributes: ["id", "name"] });
-    const expenses = await Expense.findAll({
+    const users = await User.findAll({
       attributes: [
-        "userId",
-        [sequelize.fn("sum", sequelize.col('amount')), "totalSum"]
+        "id",
+        "name",
+        [sequelize.fn("sum", sequelize.col("amount")), "totalSum"],
       ],
-      group: ["userId"],
+      include: [{ model: Expense, attributes: [] }],
+      group: ["user.id"],
+      order: [["totalSum", "DESC"]],
     });
 
-    // const aggregatedExpense={};
-    // expenses.forEach(exp=>{
-    //     if(aggregatedExpense[exp.userId]){
-    //         aggregatedExpense[exp.userId]+=exp.amount;
-    //     }
-    //     aggregatedExpense[exp.userId]=exp.amount;
-    // })
-    console.log(expenses);
-    const retArray = [];
-    // users.map((user) => {
-    //   retArray.push({
-    //     name: user.name,
-    //     totalAmount: aggregatedExpense[user.id] || 0,
-    //   });
-    // });
-    // retArray.sort((a, b) => b.totalAmount - a.totalAmount);
-    res.status(200).json({ arr:expenses });
+    res.status(200).json({ arr: users });
   } catch (error) {
     console.log(error);
     res.status(400).json({ err: error });
