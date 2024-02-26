dd980240c6174dcd95d380723ba984682da06247
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-27 01:13:46 +0530

    optimisation using attributes and sequelize.fn

--- controller/premiumFeature.js
+++ controller/premiumFeature.js
@@ -1,30 +1,37 @@
+const User = require("../model/user");
+const Expense = require("../model/expense");
+const sequelize = require("../util/database");
 
-const User= require('../model/user')
-const Expense= require('../model/expense')
+exports.getUserLeaderboard = async (req, res, next) => {
+  try {
+    const users = await User.findAll({ attributes: ["id", "name"] });
+    const expenses = await Expense.findAll({
+      attributes: [
+        "userId",
+        [sequelize.fn("sum", sequelize.col('amount')), "totalSum"]
+      ],
+      group: ["userId"],
+    });
 
-exports.getUserLeaderboard=async(req,res,next)=>{
-    try {
-        const users= await User.findAll();
-        const expenses= await Expense.findAll();
-        const aggregatedExpense={};
-        expenses.forEach(exp=>{
-            if(aggregatedExpense[exp.userId]){
-                aggregatedExpense[exp.userId]+=exp.amount;
-            }
-            aggregatedExpense[exp.userId]=exp.amount;
-        })
-        const retArray=[];
-        users.map(user=>{
-            retArray.push({name:user.name,totalAmount:aggregatedExpense[user.id] || 0})
-        })
-        retArray.sort((a,b)=>b.totalAmount-a.totalAmount)
-        res.status(200).json({arr:retArray})
-        
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({err:error})
-        
-    }
-
-
-}
+    // const aggregatedExpense={};
+    // expenses.forEach(exp=>{
+    //     if(aggregatedExpense[exp.userId]){
+    //         aggregatedExpense[exp.userId]+=exp.amount;
+    //     }
+    //     aggregatedExpense[exp.userId]=exp.amount;
+    // })
+    console.log(expenses);
+    const retArray = [];
+    // users.map((user) => {
+    //   retArray.push({
+    //     name: user.name,
+    //     totalAmount: aggregatedExpense[user.id] || 0,
+    //   });
+    // });
+    // retArray.sort((a, b) => b.totalAmount - a.totalAmount);
+    res.status(200).json({ arr:expenses });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ err: error });
+  }
+};
